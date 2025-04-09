0ec60c1b5a285a1b65b5fffb7f8a8c88da2444a6
Allie Efferson <allie.efferson@gmail.com>
2025-04-08 23:50:49 -0500

    update

---
+++ Script/gradingSystemWithArray.js
@@ -0,0 +1,30 @@
+let colors = ["green", "blue", "pink"]
+console.log (colors[0])
+console.log (colors)
+
+let person = {
+    Name: "Allie Efferson",
+    Score: 35,
+    isEnrolled: "Software Engineer"
+}
+
+let students = [
+    {Name: "Justin Hoffman", 
+    Score: 82, 
+    isEnrolled: true},
+    {Name: "Jane Smith", 
+    Score: 93, 
+    isEnrolled: false},
+    {Name: "Daniel Brown", 
+    Score: 77, 
+    isEnrolled: true},
+]
+
+console.log (students)
+console.log (students[0].Name)
+console.log (students[1].Age)
+console.log (students[2].isEnrolled)
+
+console.log(typeof students[0].Name)
+console.log(typeof students[0].Age)
+console.log(typeof students[0].isEnrolled)
