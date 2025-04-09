5ad3c4774a826ec06e9d66466512b9f5e3c41ca8
Allie Efferson <allie.efferson@gmail.com>
2025-04-09 18:29:14 -0500

    update

--- Script/gradingSystemWithArray.js
+++ Script/gradingSystemWithArray.js
@@ -1,4 +1,4 @@
-function studentGrading {studentName, studentScore} (studentName, studentScore) {
+function studentGrading {studentName, studentScore}{
 // 90-100.         A
 // 80-89.          B
 // 70-79.          C
@@ -10,6 +10,8 @@ debugger:
 if((studentScore >= 90) && (studentScore<=100)){grade = "A"}
 else if((studentScore >= 80) && (studentScore<=89)){B}
 else if((studentScore >= 70) && (studentScore<=79)){C}
+
+}
 
 console.log {"Hello ${studentName}, your score is ${studentScore} and your grade is B."}
 
