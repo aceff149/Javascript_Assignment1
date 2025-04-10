5da116f69168c41c5dbf800c8ed771faf0f945b6
Allie Efferson <allie.efferson@gmail.com>
2025-04-09 19:17:17 -0500

    update

--- Script/gradingSystemWithArray.js
+++ Script/gradingSystemWithArray.js
@@ -1,22 +1,23 @@
-function studentGrading {studentName, studentScore}{
+function studentGrading(studentName, studentScore) {
 // 90-100.         A
 // 80-89.          B
 // 70-79.          C
 
-    let studentGrade 
+debugger;
 
-
-debugger:
-if((studentScore >= 90) && (studentScore<=100)){grade = "A"}
-else if((studentScore >= 80) && (studentScore<=89)){B}
-else if((studentScore >= 70) && (studentScore<=79)){C}
-
+let studentGrade = 
+((studentScore >= 90) && (studentScore <= 100)) 
+    studentGrade = "A"
+if ((studentScore >= 80) && (studentScore <= 89))  
+    studentGrade = "B"
+else if ((studentScore >= 70) && (studentScore <= 79))
+    studentGrade = "C"
 }
+console.log("Hello", studentName, "your grade is", studentGrade)
 
-console.log {"Hello ${studentName}, your score is ${studentScore} and your grade is B."}
 
 studentGrading("Allie Efferson", 85)
 studentGrading("Jane Smith", 93)
 studentGrading("Daniel Brown", 77)
-studentGrading("Justin Hoffman", 82)
+studentGrading("Justin Hoffman", 92)
 
